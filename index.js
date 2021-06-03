e863ad0950beb342cbaec9f8e439e7697e05e1db
Thom96 <43645360+Thom96@users.noreply.github.com>
2021-06-03 23:38:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let name = "Thom";
+console.log(name);
+console.log(4+4);
+console.log("4"+"4");
+console.log(4-4);
+console.log(4*4);
+console.log(4/4);
+console.log(4%4);
+
+var age = "48";
+console.log(typeof age);
